fc0cf4f5c8651eb534ef53acd5e6e7f5632a62ab
sivakumarreddy <info@joindevops.com>
2023-10-11 08:22:47 +0530

    GIT

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ module "catalogue_instance" {
   instance_type = "t3.medium"
   vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]
   # it should be in Roboshop DB subnet
-  subnet_id = split(data.aws_ssm_parameter.private_subnet_ids,0)
+  subnet_id = element(split(",",data.aws_ssm_parameter.private_subnet_ids.value), 0)
   //user_data = file("catalogue.sh")
   tags = merge(
     {

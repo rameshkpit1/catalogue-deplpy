a2e3cfc298bfc5dbcaf3809ba80f98302f6ee3a1
rameshkpit1 <42575776+rameshkpit1@users.noreply.github.com>
2024-11-19 17:22:43 +0530

    Update catalogue.sh

--- terraform/catalogue.sh
+++ terraform/catalogue.sh
@@ -2,7 +2,12 @@
 APP_VERSION=$1
 ENVIRONMENT=$2
 echo "app version: $APP_VERSION"
-yum install python3.11-devel python3.11-pip -y
-pip3.11 install ansible botocore boto3
+sudo yum install epel-release -y
+sudo yum install https://repo.ius.io/ius-release-el7.rpm -y
+sudo yum install python39 -y
+sudo yum install python39-devel -y
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python3.9 get-pip.py
+sudo python3.9 -m pip install ansible botocore boto3
 cd /tmp
-ansible-pull -U https://github.com/sivadevopsdaws74s/ansible-roboshop-roles-tf.git -e component=catalogue -e app_version=$APP_VERSION -e env=$ENVIRONMENT main.yaml
+ansible-pull -U https://github.com/rameshkpit1/ansible-roboshop-roles-tf.git -e component=catalogue -e app_version=$APP_VERSION -e env=$ENVIRONMENT main.yaml
